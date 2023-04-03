9e578812defa0dede8b5d88146366f7edc6e460a
daochoam <daochoam@unal.edu.co>
2023-04-03 10:45:08 -0500

    cleanup

--- src/app/core/interfaces/index.ts
+++
@@ -1,7 +0,0 @@
-export { Character } from "./character";
-export { Status, Gender, Req, ReqOneCharacter, ReqCharacters } from "./request-characters";
-
-export { Episode } from "./episodes"
-export { ReqEpisode } from "./request-episodes"
-
-export { generateArrayNumbers, namesFormat, shuffleArray } from './methods'
